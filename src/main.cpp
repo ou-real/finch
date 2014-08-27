dd3cfbf494ce03bfcb63ff31925781dd41b87cc7
Braden McDorman <bmcdorman@gmail.com>
2014-08-27 14:01:59 -0500

    Added GPU info

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,16 @@ int main(int argc, char *argv[])
   
   file.close();
   
+  int gpu = 0;
+  cudaDeviceProp gpuProp;
+  cudaGetDevice(&gpu);
+  cudaGetDeviceProperties(&gpuProp, gpu);
+  cout << "GPU Information:" << endl;
+  cout << "\tName: " << gpuProp.name << "(" << gpuProp.major << "." << gpuProp.minor << ")" << endl;
+  cout << "\tMemory: " << gpuProp.totalGlobalMem << endl;
+  cout << "\tMax Threads/Block: " << gpuProp.maxThreadsPerBlock << endl;
+  cout << "\tECC Enabled: " << gpuProp.eccEnabled << endl;
+  
   builder pop_builder;
   
   novelty_fitness_mapper novelty;
